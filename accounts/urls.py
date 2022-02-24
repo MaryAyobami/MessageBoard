2df43e640db92cc0c86e6c33dd03425c6decfb8c
MaryAyobami <ogunmolamaryayobami@gmail.com>
2022-02-24 15:24:06 +0100

    commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('logout',auth_views.LogoutView.as_view(), name= 'logout'),
-    path('login',auth_views.LoginView.as_view(template_name = 'login.html' , name='login'))
+    path('login',auth_views.LoginView.as_view(template_name = 'login.html') , name='login')
     ]
